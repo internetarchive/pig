14b4c87cf35ad9cc633e709a1a7a355751b9d939
Aaron Binns <aaron@archive.org>
2013-05-27 21:37:00 +0000

    IA customization to support textinputformat.record.delimiter.force.newline property which overrides Pig built-in line terminator and uses only '\n'.  This property is true by default!

--- src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/PigTextInputFormat.java
+++ src/org/apache/pig/backend/hadoop/executionengine/mapReduceLayer/PigTextInputFormat.java
@@ -17,6 +17,34 @@
  */
 package org.apache.pig.backend.hadoop.executionengine.mapReduceLayer;
 
+// ALL INTERNETARCHIVE CHANGES INCLUDE A COMMENT STARTING "// IA "
+
+// IA Override end-of-line marker to be just '\n'
+//
+// Pig's built-in text input system uses any of 
+//    \r
+//    \r\n
+//    \n
+// as the end-of-line marker.  Often, we have text files with '\r'
+// characters in the middle of a line and don't want it to be used as
+// a line terminator.
+//
+// Setting the property:
+//
+//   textinputformat.record.delimiter.force.newline = true
+//
+// will over-ride Pig's normal defaults and will *only* use '\n' as
+// the line terminator.  Setting the property to 'false' will restore
+// Pig's built-in behavior.
+// 
+// The default value is 'true', so we are changing the default
+// behavior of Pig.
+//
+// NOTE: We cannot simply set the 'textinputformat.record.delimiter'
+// property in a Pig script because there's no way to embed a '\n'
+// character in a Pig 'set' statement.
+
+
 import java.io.IOException;
 import java.util.List;
 
@@ -25,9 +53,34 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.pig.backend.hadoop.executionengine.util.MapRedUtil;
 
+// IA Imports needed by our customized createRecordReader method
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
 public class PigTextInputFormat extends TextInputFormat {
 
-    /*
+  // IA Begin
+  /**
+   * Set the 'textinputformat.record.delimiter' to a custom value if
+   * the IA-specific override property is true.
+   */
+  @Override
+  public RecordReader<LongWritable, Text> createRecordReader( InputSplit split, TaskAttemptContext context )
+  {
+    // Force parent reader to *only* use \n for eol, not \r
+    if ( context.getConfiguration().getBoolean( "textinputformat.record.delimiter.force.newline", true ) )
+      {
+        context.getConfiguration().set( "textinputformat.record.delimiter", "\n" );
+      }
+    
+    return super.createRecordReader( split, context );
+  }
+  // IA End
+
+   /*
      * This is to support multi-level/recursive directory listing until 
      * MAPREDUCE-1577 is fixed.
      */
